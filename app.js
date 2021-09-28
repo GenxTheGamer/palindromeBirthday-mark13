8ef01ac2c092a998076831d2e2a0398ab76ca737
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-28 10:19:25 +0530

    added dateFromNumberToString function ex03

--- app.js
+++ app.js
@@ -10,4 +10,31 @@ function isPalindrome(str) {
   return str === reverseList;
 }
 
-console.log(isPalindrome("lool"));
+// console.log(isPalindrome("lool"));
+
+function dateFromNumberToString(date) {
+  var dateStr = { day: "", month: "", year: "" };
+
+  if (date.day < 10) {
+    dateStr.day = "0" + date.day;
+  } else {
+    dateStr.day = date.day.toString();
+  }
+
+  if (date.month < 10) {
+    dateStr.month = "0" + date.month;
+  } else {
+    dateStr.month = date.month.toString();
+  }
+
+  dateStr.year = date.year.toString();
+
+  return dateStr;
+}
+
+var date = {
+  day: 10,
+  month: 2,
+  year: 2020,
+};
+console.log(dateFromNumberToString(date));
